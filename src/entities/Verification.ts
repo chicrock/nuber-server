018b7262f6edcdc0b5059c20060f29e69f5ae9b0
ChicRock <jungsungrak@gmail.com>
2018-09-09 14:29:35 +0900

    1-19 Creating the verification key

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -5,15 +5,19 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  BeforeInsert,
 } from "typeorm";
 import { verificationTarget } from "../types/types";
+
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
 
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  @Column({ type: "text", enum: [PHONE, EMAIL] })
   target: verificationTarget;
 
   @Column({ type: "text" })
@@ -27,8 +31,20 @@ class Verification extends BaseEntity {
 
   @CreateDateColumn()
   createdAt: string;
+
   @UpdateDateColumn()
   updatedAt: string;
+
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(Math.random() * 100000).toString();
+    } else if (this.target === EMAIL) {
+      this.key = Math.random()
+        .toString(36)
+        .substr(2);
+    }
+  }
 }
 
 export default Verification;
