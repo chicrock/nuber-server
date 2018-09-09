e23316a4d5aebdc5841c84b8f4060570dac67c36
ChicRock <jungsungrak@gmail.com>
2018-09-09 14:21:55 +0900

    1-18 Add predev and Using Types on the Entities

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -6,16 +6,19 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { verificationTarget } from "../types/types";
 
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "text" })
-  target: string;
+  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  target: verificationTarget;
+
   @Column({ type: "text" })
   payload: string;
+
   @Column({ type: "text" })
   key: string;
 
