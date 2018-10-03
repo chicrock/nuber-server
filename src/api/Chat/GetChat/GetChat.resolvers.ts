3458e6a41fde1a31e3f5c8a3442801791ccd5bc3
ChicRock <jungsungrak@gmail.com>
2018-10-03 19:57:28 +0900

    1-82 GetChat resolver

---
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -0,0 +1,52 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { GetChatResponse, GetChatQueryArgs } from "../../../types/graph";
+import User from "../../../entities/User";
+import Chat from "../../../entities/Chat";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetChat: privateResolver(
+      async (_, args: GetChatQueryArgs, { req }): Promise<GetChatResponse> => {
+        const user: User = req.user;
+        const { chatId } = args;
+
+        try {
+          const chat = await Chat.findOne({
+            id: chatId,
+          });
+
+          if (chat) {
+            if (chat.driverId === user.id || chat.passengerId === user.id) {
+              return {
+                ok: true,
+                error: null,
+                chat,
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Not authorized to see the chat",
+                chat: null,
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "Chat not found",
+              chat: null,
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            chat: null,
+          };
+        }
+      }
+    ),
+  },
+};
+
+export default resolvers;
